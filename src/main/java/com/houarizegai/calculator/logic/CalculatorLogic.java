32d1a8722bfc0465e99a00747cf76c2147d15320
Dory Apollon <>
2024-06-02 17:30:40 -0400

    Added Pi functionality to the calculator

---
+++ src/main/java/com/houarizegai/calculator/logic/CalculatorLogic.java
@@ -0,0 +1,35 @@
+package com.houarizegai.calculator.logic;
+
+public class CalculatorLogic {
+
+    public double calculate(double firstNumber, double secondNumber, char operator) {
+        switch (operator) {
+            case '+':
+                return firstNumber + secondNumber;
+            case '-':
+                return firstNumber - secondNumber;
+            case '*':
+                return firstNumber * secondNumber;
+            case '/':
+                return firstNumber / secondNumber;
+            case '%':
+                return firstNumber % secondNumber;
+            case '^':
+                return Math.pow(firstNumber, secondNumber);
+            default:
+                return secondNumber;
+        }
+    }
+
+    public double sqrt(double number) {
+        return Math.sqrt(number);
+    }
+
+    public double log(double number) {
+        return Math.log(number);
+    }
+    public double PI(double number) {
+        return Math.PI * number;
+    }
+}
+
